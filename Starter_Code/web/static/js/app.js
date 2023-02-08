efcdf6428a8f12528dbfcbe4bf71b7fa6a8ea46d
UnBearAble1 <jeremiah.slinde@yahoo.com>
2023-02-08 11:46:11 -0600

    Working on the module

--- Starter_Code/web/static/js/app.js
+++ Starter_Code/web/static/js/app.js
@@ -30,11 +30,13 @@ function buildTable(data) {
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
+    let element = d3.selectAll()
 
     // 4b. Save the value that was changed as a variable.
+    let value = 
 
     // 4c. Save the id of the filter that was changed as a variable.
-
+    let id = d3.
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
@@ -56,11 +58,12 @@ function updateFilters() {
     
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("#filter-btn").on("change", updateFilters);
+
   
   // Build the table when the page loads
   buildTable(tableData);
